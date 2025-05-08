9daeeb2abab6da37cbc8d388cb7da0b1b763ff92
leegangjin <dkakvhfh@naver.com>
2025-05-08 09:40:24 +0900

    2025/05/08 파일업로드 테스트

---
+++ src/main/java/com/project/trable/Controller/myController.java
@@ -0,0 +1,12 @@
+package com.project.trable.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class myController {
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
+}
---
+++ src/test/java/com/project/trable/TestShareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShareProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
